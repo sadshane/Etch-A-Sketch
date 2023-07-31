2d1bbeb815054f6a9afc57f65294c4a62cb9552c
Shane <71f4ldre@duck.com>
2023-07-31 19:24:09 +0800

    appended divs for individual pixels

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Shania Claire');
+
+
+function createPixels() {
+    let container = document.querySelector('.container');
+    for (let i = 0; i < 16; i++)
+    {
+        let pixel = document.createElement('div');
+        let styles = 'height: 100px; width: 100px; background-color: blue;';
+        pixel.setAttribute('style', styles);
+        container.appendChild(pixel);
+    }
+}
+function main() {
+    createPixels();
+}
+
+main();
