8473585a0f8cf20f82882e173f363d559ed37255
Shane <71f4ldre@duck.com>
2023-08-01 07:18:25 +0800

    making the main logic of the project

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 console.log('Shania Claire');
 
 
-function createPixels() {
+function createPixels(size) {
     let container = document.querySelector('.container');
-    for (let i = 0; i < 16; i++)
+    for (let i = 0; i < size; i++)
     {
-        let pixel = document.createElement('div');
-        let styles = 'height: 100px; width: 100px; background-color: blue;';
-        pixel.setAttribute('style', styles);
-        container.appendChild(pixel);
+        let row = document.createElement('div');
+        row.setAttribute('style', 'margin: 4px 0px; display: flex; gap: 4px;')
+        for (let j = 0; j < size; j++)
+        {
+            let pixel = document.createElement('div');
+            let styles = 'height: 50px; width: 50px; background-color: blue;';
+            pixel.setAttribute('style', styles);
+            row.appendChild(pixel);
+
+            pixel.addEventListener('mouseenter', () => {
+                pixel.style.backgroundColor = 'red';
+            });
+        }
+        container.appendChild(row);
     }
+    
 }
+
 function main() {
-    createPixels();
+    createPixels(16);
 }
 
 main();
